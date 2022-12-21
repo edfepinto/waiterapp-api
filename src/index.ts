cebe4120b5e2534b0dcdc9d25f07df70cc51c951
Eduardo Pinto <edfepinto@gmail.com>
2022-12-21 14:26:10 -0300

    add web sockets

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import path from 'node:path';
-
+import http from 'node:http';
 import express from 'express';
 import mongoose from 'mongoose';
+import { Server } from 'socket.io';
 
 import { router } from './routes';
 
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
-    const app = express();
-
     app.use((req, res, next) => {
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Methods', '*');
@@ -19,7 +22,7 @@ mongoose.connect('mongodb://localhost:27017')
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
-    app.listen(3001, () => {
+    server.listen(3001, () => {
       console.log('🚀 Server is running on http://localhost:3001');
     });
   })
